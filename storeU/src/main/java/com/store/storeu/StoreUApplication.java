0d40dcb74055c9245bc4c1b057178656df98ed42
lyc4nthrope <osoriocristhian86@gmail.com>
2025-04-17 20:19:42 -0500

    se me olvido que era en ingles borre y volvi a empezar
    store university v1

---
+++ storeU/src/main/java/com/store/storeu/StoreUApplication.java
@@ -0,0 +1,13 @@
+package com.store.storeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreUApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreUApplication.class, args);
+    }
+
+}
---
+++ storeU/src/test/java/com/store/storeu/StoreUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.storeu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreUApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
